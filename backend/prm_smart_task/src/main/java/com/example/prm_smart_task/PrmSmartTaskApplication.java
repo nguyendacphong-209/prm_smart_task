6e3d7090ab28ce250345874ea30affee17421e1c
Nguyễn Đắc Phong <dacphong2092003@gmail.com>
2026-03-19 13:58:49 +0700

    feat: add spring boot backend base

---
+++ backend/prm_smart_task/src/main/java/com/example/prm_smart_task/PrmSmartTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.prm_smart_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrmSmartTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrmSmartTaskApplication.class, args);
+	}
+
+}
---
+++ backend/prm_smart_task/src/test/java/com/example/prm_smart_task/PrmSmartTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prm_smart_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrmSmartTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
